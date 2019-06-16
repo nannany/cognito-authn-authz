a3954f6cd5402ddd497a02346f8109fa6ce1ce20
nannany <ymym1990ymym@gmail.com>
2019-06-16 21:29:45 +0900

    login.vueコミット

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+        <v-text-field v-model="name" label="name" type="text"></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="パスワード"
+          type="password"
+        ></v-text-field>
+      <v-btn class="primary" dark @click.stop="login()">ログイン</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Amplify from 'aws-amplify';
+Amplify.configure({
+    Auth: {
+        identityPoolId: '',
+        region: '',
+        userPoolId: '',
+        userPoolWebClientId: ''
+    }
+})
+</script>
